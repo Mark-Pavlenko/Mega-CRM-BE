51691fb2aa29484e8e5473ec89d1ee42a0eaea83
marko.pavlenko <mark.pavlenko@megadevllc.com>
2022-05-13 16:17:02 +0300

    feat: update employee req to receive incentoryUnit"

--- src/controllers/WarehouseController.js
+++ src/controllers/WarehouseController.js
@@ -50,19 +50,36 @@ class WarehouseController {
   static async processGetInventoryRequest(req, res, next) {
     // console.log("createGetEquipmentRequest body", req.body);
 
-    let necessaryUser = await EmployeeModel.find({
-      "requests._id": req.body.requestId,
-    });
-
-    let selectedRequest = necessaryUser[0].requests.find((request) => {
-      return request._id == req.body.requestId;
-    });
-
-    let test1 = JSON.parse(JSON.stringify(selectedRequest));
-    console.log({
-      ...test1,
-      requestStatus: "Accepted",
-    });
+    // let necessaryUser = await EmployeeModel.find({
+    //   "requests._id": req.body.requestId,
+    // });
+    //
+    // let selectedRequest = necessaryUser[0].requests.find((request) => {
+    //   return request._id == req.body.requestId;
+    // });
+    //
+    // let test1 = JSON.parse(JSON.stringify(selectedRequest));
+    // console.log({
+    //   ...test1,
+    //   requestStatus: "Accepted",
+    // });
+    //
+    // //update request in user body
+    //
+    // const filter = {};
+    await EmployeeModel.update(
+      { "requests._id": req.body.requestId },
+      {
+        $set: { "requests.$.requestStatus": req.body.requestStatus },
+      }
+      // { new: true }
+    )
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
   }
 }
 
